1d2f7ec180243d65a11328fe43e003a18854ec01
nvolve <snevarez89@gmail.com>
2018-02-20 21:56:14 -0800

    init push

---
+++ nuxt.config.js
@@ -0,0 +1,26 @@
+const resolve = require('path').resolve;
+
+module.exports = {
+	css: [
+		'normalize.css',
+		{src: '@/assets/styles/main.scss', lang: 'scss'}
+	],
+	modules: [
+		// Simple usage
+		'@nuxtjs/dotenv',
+		['nuxt-sass-resources-loader', resolve(__dirname, 'assets/styles/main.scss')],
+	],
+	generate: {
+		fallback: true, // if you want to use '404.html'
+	},
+	head: {
+		title: "Sam Nevarez - Portfolio",
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' }
+		],
+		link: [
+			{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Lato:400,700|Roboto+Condensed:400,700' }
+		]
+	}
+}
---
+++ pages/index.vue
@@ -0,0 +1,89 @@
+<template>
+	<div>
+		<div class="background-image">
+			<div class="container">
+				<div class="content">
+
+					<div class="headshot">
+					</div>
+
+					<h1 class="title">
+						<div>
+							Welcome to the
+						</div>
+
+						<div>
+							Portfolio of Sam Nevarez
+						</div>
+					</h1>
+
+					<router-link to="about">
+						Enter Portfolio
+					</router-link>
+
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+
+<script>
+import axios from 'axios';
+
+export default {
+	// asyncData(context) {
+	// 	return axios.get(`https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE}/entries?access_token=${process.env.CONTENTFUL_KEY}`)
+	// 	.then(resp => {
+	// 		return {
+	// 			blogPosts: resp.data.items,
+	// 		}
+	// 	});
+	// },
+
+	data: function() {
+		return {}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+
+	.background-image {
+		background-image: url('~/images/blue-background.png');
+		position: absolute;
+		z-index: -1;
+		left: 0;
+		right: 0;
+		top: 0;
+		bottom: 0;
+		display: flex;
+		justify-content: center;
+	}
+
+	.container {
+		align-self: center;
+		padding: 0 15px;
+	}
+
+	.content {
+		text-align: center;
+		text-transform: uppercase;
+	}
+
+	.headshot {
+		background-image: url('~/images/sam.png');
+		background-size: cover;
+		margin: 0 auto 46px;
+		height: 210px;
+		width: 210px;
+	}
+
+	.title {
+		margin: 0 0 30px;
+		font-size: 32px;
+		color: $blue;
+	}
+
+</style>
+
