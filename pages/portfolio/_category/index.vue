1dc76027488b920ab50164d4d6f5c36d589fef81
AJ <ajs4392@gmail.com>
2018-06-04 17:53:19 -0400

    UPDATE category styles

--- pages/portfolio/_category/index.vue
+++ pages/portfolio/_category/index.vue
@@ -1,11 +1,20 @@
 <template>
-    <div>
-        <nuxt-link v-for="piece in portfolioPieces" :key="piece.fields.slug" :to="`/portfolio/${$route.params.category}/${piece.fields.slug}`" class="link">
-            <h2 class="title">
-                {{ piece.fields.title }}
-            </h2>
-        </nuxt-link>
-    </div>
+  <div>
+    <nuxt-link v-for="piece in portfolioPieces" :key="piece.fields.slug" :to="`/portfolio/${$route.params.category}/${piece.fields.slug}`" class="link">
+      <h2 class="title">
+        {{ piece.fields.title }}
+      </h2>
+      <div class="excerpt-container">
+        <p class="excerpt">
+          {{ piece.fields.excerpt }}
+        </p>
+
+        <div class="read-more">
+          Read More
+        </div>
+      </div>
+    </nuxt-link>
+  </div>
 
 </template>
 
@@ -32,3 +41,52 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.link {
+  color: #fff;
+  display: block;
+
+  @include bp("md") {
+    display: flex;
+  }
+}
+
+.title {
+  font-size: 20px;
+  font-weight: 300;
+  margin: 0;
+  background-color: $blue;
+  padding: 16px;
+  text-transform: uppercase;
+
+  @include bp("md") {
+    flex: 0.333333;
+  }
+}
+
+.excerpt-container {
+  background-color: rgba(0, 42, 92, 0.8);
+  font-size: 18px;
+  font-family: $latoFont;
+  padding: 16px 16px;
+
+  @include bp("md") {
+    flex: 0.666666;
+  }
+}
+
+.excerpt {
+  margin: 0 0 40px;
+  font-size: inherit;
+  line-height: 24px;
+  text-align: justify;
+}
+
+.read-more {
+  color: $blue;
+  text-transform: uppercase;
+  text-decoration: underline;
+  text-align: right;
+}
+</style>
